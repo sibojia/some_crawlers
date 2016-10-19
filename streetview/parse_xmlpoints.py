819d8a25f8b5b1f72c8e479c15d57206751d9a0b
sibojia <jiasibo@sensetime.com>
2016-10-19 19:00:34 +0800

    support gps query when showing xml image

--- streetview/parse_xmlpoints.py
+++ streetview/parse_xmlpoints.py
@@ -56,7 +56,14 @@ def get_point_image(points, width=800, height=600):
         x=(p[0][0]-minbound[0])*width/x_delta
         y=height-(p[0][1]-minbound[1])*height/y_delta
         img[y,x]=(255,150,150)
-    return img
+    def f_get_pos(ix, iy):
+        return (ix*x_delta/width+minbound[0], (height-iy)*y_delta/height+minbound[1])
+    return img, f_get_pos
+
+def on_mouse(event,x,y,flags,f_get_pos):
+    if event==cv2.EVENT_LBUTTONDBLCLK:
+        gx,gy=f_get_pos(x,y)
+        print "(%d, %d) -> (%f, %f)" % (x,y,gx,gy)
 
 def main():
     if False:
@@ -64,10 +71,11 @@ def main():
         cPickle.dump(points, open('pdata-tmp.pkl','wb'), cPickle.HIGHEST_PROTOCOL)
     else:
         points=cPickle.load(open(sys.argv[1]))
-    img=get_point_image(points, width=8192, height=8192)
-    # cv2.imshow("map",img)
+    img,f_get_pos=get_point_image(points, width=1024, height=1024)
     cv2.imwrite("pmap.png", img)
-    # cv2.waitKey(0)
+    cv2.imshow("map",img)
+    cv2.setMouseCallback('map',on_mouse,f_get_pos)
+    cv2.waitKey(0)
 
 if __name__ == "__main__":
     main()
